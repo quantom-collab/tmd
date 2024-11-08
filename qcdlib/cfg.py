156d67af7c695c06af3249838a5426977d311b88
pcbarry <barryp@jlab.org>
2024-11-07 19:54:50 -0500

    adding evolution code and OPE FF

--- qcdlib/cfg.py
+++ qcdlib/cfg.py
@@ -6,4 +6,5 @@ alphaS_order=1 #! 0:LO  1: NLO
 dglap_order=1  #! 0:LO  1: NLO
 idis_order=1   #! 0:LO  1: NLO
 tmd_order=1    #! 0:LO  1: NLO
+tmd_resummation_order = 2 #! 0: LL, 1: NLL, 2: NNLL
 Q20=par.mc2
